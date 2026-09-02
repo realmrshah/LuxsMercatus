8f63bd261cf2963eaf0ab20f4cf74519c4cbafa2
Sha H <151533782+realmrshah@users.noreply.github.com>
2026-09-02 14:41:49 +0530

    Update footer text and contact information

--- index.php
+++ index.php
@@ -446,7 +446,7 @@ $('.navbar-light .dmenu').hover(function () {
                     <div class="footer-text">
                         <p>
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
- &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made  <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://instagram.com/_.hedonist._" target="_blank">Lucifiuer</a>
+ &copy;<script>document.write(new Date().getFullYear());</script> Contact Us: +91 | Made by realmrshah <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://instagram.com/_.hedonist._" target="_blank">Lucifiuer</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                         </p>
                     </div>
