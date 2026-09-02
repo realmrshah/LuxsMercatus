91b72b6196683a1f85145fba65c5a9ffb3b6368b
Sha H <151533782+realmrshah@users.noreply.github.com>
2026-09-02 14:45:30 +0530

    Update footer contact information and attribution

--- cat_detail.php
+++ cat_detail.php
@@ -161,7 +161,7 @@ $result=select($q);
                     <div class="footer-text">
                         <p>
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> Contact us +91 7204873414 | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href=https://instagram.com/_.hedonist._/" target="_blank">LUCIFIEUR</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Contact Us: +91 | Made by realmrshah <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href=https://instagram.com/_.hedonist._/" target="_blank">LUCIFIEUR</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                         </p>
                     </div>
